2f82a08aa232065ee6bfc7c73aa82b9b6b151a3f
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-11-26 11:04:27 -0400

    deleted first day kata

--- Gol.scala
+++ Gol.scala
@@ -2,18 +2,4 @@ package Gol
 
 object Gol extends App {
 
-
-  def setNewState(state: Int, neighbors: Int) : Int = {
-    if (state == 1 && neighbors < 2) {
-      return 0
-    } else if (state == 1 && neighbors > 3) {
-      return 0
-    } else if (state == 1 && (neighbors == 2 || neighbors == 3)) {
-      return 1
-    } else if (state == 0 && neighbors == 3) {
-      return 1
-    } else {
-      return state
-    }
-  }
 }
