246d94bde3fb1d79f7dd1897a73bab8d8af9b4d0
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-11-25 22:24:24 -0400

    created main testing functions

--- Gol.scala
+++ Gol.scala
@@ -1,11 +1,19 @@
 package Gol
 
 object Gol extends App {
-  def setNewState(state: Int) : Int = {
-    if (state == 0) {
+
+
+  def setNewState(state: Int, neighbors: Int) : Int = {
+    if (state == 1 && neighbors < 2) {
+      return 0
+    } else if (state == 1 && neighbors > 3) {
+      return 0
+    } else if (state == 1 && (neighbors == 2 || neighbors == 3)) {
+      return 1
+    } else if (state == 0 && neighbors == 3) {
       return 1
     } else {
-      return  0
+      return state
     }
   }
 }
